c21c3e60849995be4ffe208da1aeca1ff582a85a
庄金鑫 <zhuangjinxin@hotmail.com>
2016-12-01 20:40:08 +0800

    feat: spring boot 连接redis

--- src/main/java/com/zhuangjinxin/demo/logback/SampleLogbackSlf4jApplication.java
+++ src/main/java/com/zhuangjinxin/demo/logback/SampleLogbackSlf4jApplication.java
@@ -13,20 +13,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class SampleLogbackSlf4jApplication {
 	
-	private static final Logger logger = LoggerFactory.getLogger(SampleLogbackSlf4jApplication.class);
+	//private static final Logger logger = LoggerFactory.getLogger(SampleLogbackSlf4jApplication.class);
 	
 	//@PostConstruct
 	@Scheduled(fixedDelay=3000)
 	public void logSomething(){
 		System.out.println("-------开始打印日志------");
-		logger.info("Info Message");
-		logger.debug("Debug Message");
-		logger.trace("Trace Message");
+		//logger.info("Info Message");
+		//logger.debug("Debug Message");
+		//logger.trace("Trace Message");
 		System.out.println("-------结束打印日志------");
 	}
 	
 	public static void main(String[] args){
-		SpringApplication.run(SampleLogbackSlf4jApplication.class, args).close();;
+		SpringApplication.run(SampleLogbackSlf4jApplication.class, args);;
 	}
 
 }
